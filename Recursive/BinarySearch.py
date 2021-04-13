4aeb40ca4a249bbbce353ce8b1e09736a86b987c
seungmoo.lee <seungmoo.lee@lotte.net>
2021-04-13 21:36:57 +0900

    하노이의 탑 풀이 추가, 이진탐색 한번 더 풀기, 개같은거 ㅠㅠ

--- Recursive/BinarySearch.py
+++ Recursive/BinarySearch.py
@@ -5,15 +5,18 @@ def binary_search(element, some_list, start_index=0, end_index=None):
         end_index = len(some_list) - 1
     # 코드를 작성하세요.
     mid_index = (start_index + end_index) // 2
-    if start_index <= end_index:
-        if some_list[mid_index] == element:
-            return mid_index
-        elif some_list[mid_index] > element:
-            return binary_search(element, some_list, start_index, mid_index - 1)
-        else:
-            return binary_search(element, some_list, mid_index + 1, end_index)
-    else:
+    # 돌리다가 결국 start_index가 커지는 경우 (해당 엘리먼트가 없다는 것)
+    if start_index > end_index:
         return None
+    # 동일한 경우 바로 리턴
+    if some_list[mid_index] == element:
+        return mid_index
+    # 왼쪽으로 탐색
+    elif some_list[mid_index] > element:
+        return binary_search(element, some_list, start_index, mid_index - 1)
+    # 오른쪽으로 탐색
+    else:
+        return binary_search(element, some_list, mid_index + 1, end_index)
 
 
 print(binary_search(2, [2, 3, 5, 7, 11]))
--- Recursive/HanoiTop.py
+++ Recursive/HanoiTop.py
@@ -1,10 +1,32 @@
+"""
+개가튼 하노이의 탑
+
+아래와 같은 패턴으로 하면 된다. 개같은거...
+
+<1 1 3>
+--> <0 1 x> <1 1 3> <0 x 3>
+
+<2 1 3>
+--> <1 1 2> + <2 1 3>  + <1 2 3>
+
+<3 1 3>
+--> <2 1 2> + <3 1 3> + <2 2 3>
+
+<4 1 3>
+--> <3 1 2> + <4 1 3> + <3 2 3>
+
+<5 1 3>
+--> <4 1 2> + <5 1 3> + <4 2 3>
+
+"""
+
 def move_disk(disk_num, start_peg, end_peg):
     print("%d번 원판을 %d번 기둥에서 %d번 기둥으로 이동" % (disk_num, start_peg, end_peg))
 
 def hanoi(num_disks, start_peg, end_peg):
     # 코드를 입력하세요.
-    if num_disks == 1:
-        move_disk(num_disks, start_peg, end_peg)
+    if num_disks == 0:
+        return
     else:
         mid_peg = 6 - start_peg - end_peg
         hanoi(num_disks - 1, start_peg, mid_peg)
