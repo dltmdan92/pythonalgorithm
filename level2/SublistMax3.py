f25068cf3d39a6e24144895f7da3e7912d271f00
dltmdan92 <dltmdan92@naver.com>
2021-05-11 20:13:43 +0900

    O(n)의 성능을 내도록 so_far(지금까지의) min,max 값을 캐싱하기

--- level2/SublistMax3.py
+++ level2/SublistMax3.py
@@ -1,3 +1,14 @@
+"""
+실습과제
+100XP
+이미 sublist_max 함수를 각각 Brute Force과 Divide and Conquer 방식으로 작성했는데요.
+Brute Force로 풀었을 때는 시간 복잡도가 O(n^2), Divide and Conquer를 사용했을 때는 O(nlgn)였습니다.
+
+이번 과제에서는 시간 복잡도를 O(n)로 한 번 더 단축해보세요!
+
+과제 설명은 저번 챕터를 참고하세요!
+"""
+
 # O(n)으로 풀어보기
 def sublist_max(profits):
     # 코드를 작성하세요.
